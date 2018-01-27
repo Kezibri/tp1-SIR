a66fe534864c4c8b84dea26cd8d293df9191a2e9
Kezibri Anas <anas.kezibri@gmail.com>
2018-01-27 15:06:21 +0100

    code commiting

---
+++ src/main/java/tp1gid/tp1afid/App.java
@@ -0,0 +1,19 @@
+package tp1gid.tp1afid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void deadCode()
+	{
+		int i=5;
+		System.out.println( i );
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
